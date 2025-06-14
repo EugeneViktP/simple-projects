64b13e75f40ebe9b3750971d8b5e6e05e560a679
EugeneViktP <popoff.zhenya@yandex.ru>
2025-06-14 17:09:04 +0300

    updated game

--- game.c
+++ game.c
@@ -4,18 +4,19 @@ int main(void)
 {
     char color[20];
     char pluralNoun[20];
-    char celebrity[20];
+    char celebrityF[20];
+    char celebrityL[20];
 
     printf("Enter a color: ");
     scanf("%s", color);
-    printf("Enter a plural nout: ");
+    printf("Enter a plural noun: ");
     scanf("%s", pluralNoun);
     printf("Enter a celebrity: ");
-    scanf("%s", celebrity);
+    scanf("%s%s", celebrityF, celebrityL);
     
     printf("Rosese are %s\n", color);
     printf("%s are blue\n", pluralNoun);
-    printf("I love %s\n", celebrity);
+    printf("I love %s %s\n", celebrityF, celebrityL);
 
     return 0;
 }
