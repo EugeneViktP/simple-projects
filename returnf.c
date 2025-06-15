bb365cceb574e5bc37e1b9277c44fe3bdba82459
EugeneViktP <popoff.zhenya@yandex.ru>
2025-06-15 11:07:12 +0300

    updated returnf

--- returnf.c
+++ returnf.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 
-double cube(double num) {
-    double result = num * num * num;
-    return result;
-    printf("Here");
-}
+double cube(double num);
 
 int main(void)
 {
     printf("Answer: %f", cube(3.0));
     return 0;
+}
+
+double cube(double num) {
+    double result = num * num * num;
+    return result;
+    printf("Here");
 }
