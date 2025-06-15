bf5412033c60ab1bd449945a355424ab0e365a3b
EugeneViktP <popoff.zhenya@yandex.ru>
2025-06-15 18:07:06 +0300

    updated version of calculator

--- calculator.c
+++ calculator.c
@@ -4,9 +4,30 @@ int main(void)
 {
     double num1;
     double num2;
-    printf("Enter numbers to be summed up: ");
-    scanf("%lf %lf", &num1, &num2);
-    printf("The sum of these numbers %f", num1 + num2);
+    char op;
+
+    printf("Enter a number: ");
+    scanf("%lf", &num1);
+    printf("Enter operator:");
+    scanf(" %c", &op);
+    printf("Enter a number: ");
+    scanf("%lf", &num2);
+    switch(op) {
+        case '+':
+        printf("Sum: %f", num1 + num2);
+        break;
+        case '-':
+        printf("Diff: %f", num1 - num2);
+        break;
+        case '*':
+        printf("Product: %f", num1 * num2);
+        break;
+        case '/':
+        printf("Quotient: %f", num1 / num2);
+        break;
+        default:
+        printf("Not supported op");
+    }
 
     
     return 0;
