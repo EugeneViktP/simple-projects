5f5f48a6bfa31d32adeef0f81d806d55971b5487
EugeneViktP <popoff.zhenya@yandex.ru>
2025-06-23 21:14:44 +0300

    pointers in functions with arrays

---
+++ point_arr_funct.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int SumOfElements(int A[])
+{
+    int i, sum = 0;
+    int size = sizeof(A)/sizeof(A[0]);
+    printf("Size of A = %d, and size of A[0] = %d\n", sizeof(A), sizeof(A[0]));
+    for (i = 0; i < size; i++) {
+        sum += A[i];
+    }
+    return sum;
+
+}
+
+int main(void)
+{
+    int A[] = {1, 2, 3, 4, 5};
+    
+    // int size = sizeof(A)/sizeof(A[0]);
+    int total = SumOfElements(A);
+    printf("Size of A = %d, and size of A[0] = %d\n", sizeof(A), sizeof(A[0]));
+    // printf("%d\n", size);
+    printf("%d\n", total);
+    
+    return 0;
+}
