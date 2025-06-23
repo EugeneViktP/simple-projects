4f9d7c2f055aeeefdcb664d9f4383aeffa1ccfcb
EugeneViktP <popoff.zhenya@yandex.ru>
2025-06-23 21:26:01 +0300

    functions on arrays

--- point_arr_funct.c
+++ point_arr_funct.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int SumOfElements(int A[])
+int SumOfElements(int A[], int size)
 {
     int i, sum = 0;
-    int size = sizeof(A)/sizeof(A[0]);
-    printf("Size of A = %d, and size of A[0] = %d\n", sizeof(A), sizeof(A[0]));
+    // int size = sizeof(A)/sizeof(A[0]);
+    // printf("Size of A = %d, and size of A[0] = %d\n", sizeof(A), sizeof(A[0]));
     for (i = 0; i < size; i++) {
         sum += A[i];
     }
@@ -16,8 +16,8 @@ int main(void)
 {
     int A[] = {1, 2, 3, 4, 5};
     
-    // int size = sizeof(A)/sizeof(A[0]);
-    int total = SumOfElements(A);
+    int size = sizeof(A)/sizeof(A[0]);
+    int total = SumOfElements(A, size);
     printf("Size of A = %d, and size of A[0] = %d\n", sizeof(A), sizeof(A[0]));
     // printf("%d\n", size);
     printf("%d\n", total);
