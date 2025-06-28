85d702f804f6249fdf3fafebdc04d974a491fee9
EugeneViktP <popoff.zhenya@yandex.ru>
2025-06-28 20:45:10 +0300

    multi dimensional arrays with pointer

--- multi_arr_function.c
+++ multi_arr_function.c
@@ -2,23 +2,39 @@
 
 int main(void)
 {
-     int A[2][3] = 
+     int A[3][2][2] = {
+    
+        {
+        {76, 2},
+        {3, 4},
+    },
     {
-        {1, 2, 3},
-        {4, 5, 6}
+        {5, 6},
+        {7, 8},
+    },
+    {
+        {9, 10},
+        {11, 12},
+    }
     };
 
-    int (*p)[3] = A;
+    int (*p)[2][2] = A;
 
-    printf("%d\n", A);
+    printf("%d\n", A); //pointer to the first element of the first array
     printf("%d\n", *A);
     printf("%d\n", A[0]);
-    printf("%d\n", *p);
+    printf("%d\n", *A[0][1]);
+    printf("This address %d\n", *(p + 1));
+    printf("This address %d\n", *p + 1);
+    printf("This address %d\n", *(*p) + 1);
+    printf("This address %d\n", **(*p) + 1);
+    printf("This address %d\n", *(A[0][1] + 1));
+    printf("This address %d\n", *(A[1] + 1));
 
-    printf("%d\n", (*(p + 1) + 1));
-    printf("%d\n", *(*(p + 1) + 1));
+    // printf("Value or pointer = %d\n", (*(p + 1) + 1)); //Арифметика указателей
+    // printf("%d\n", *(*(p + 1) + 1));
     
-    printf("%d\n", (*(p + 1) + 2));
-    printf("%d\n", *(*(p + 1) + 2));
+    // printf("%d\n", (*(p + 1) + 2));
+    // printf("%d\n", *(*(p + 1) + 2));
     return 0;
 }
