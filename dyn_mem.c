1bc497cb6b95ba764bfbcaf4b1ef0ce3645cd0e9
EugeneViktP <popoff.zhenya@yandex.ru>
2025-06-29 21:01:46 +0300

    dynamic memory allocation

---
+++ dyn_mem.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void)
+{
+    int n;
+    printf("Enter size of array\n");
+    scanf("%d",&n);
+
+    // int A[n];
+    int *A = (int*)malloc(n*sizeof(int)); //dynamically allocated array
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = i + 1;
+    }
+    // free(A);
+    // A[2] = 77;
+
+    // A = NULL;
+
+    int *B = realloc(A, 2*n*sizeof(int));
+
+    printf("Prev block adress = %d, new address = %d\n", A, B);
+    for (int i = 0; i < 2*n; i++)
+    {
+        printf("%d ", B[i]);
+    }
+    // free(A);
+
+
+    
+    return 0;
+}
