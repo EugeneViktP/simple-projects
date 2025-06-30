c61f7b5fbd0543d806626afaa4f9f53619f36037
EugeneViktP <popoff.zhenya@yandex.ru>
2025-06-30 19:22:44 +0300

    pointers ad functions return

---
+++ point_as_fret.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* Add(int* a, int* b) // pointer to integer in called function
+{
+    printf("address of a in Add = %d\n", a);
+    printf("dereferencing = %d\n", *a);
+    printf("address of pointer = %d\n", &a);
+    int* c =(int*)malloc(sizeof(int));
+    printf("address of c pointer = %d\n", c);
+    *c = *a + *b;
+    return c;
+}
+
+int main(void)
+{
+    int a = 2, b = 4;
+    printf("address of a in main = %d\n", &a);
+    int* ptr = Add(&a, &b); // value in x of main is copied to a of Add
+    printf("Sum = %d, at address = %d", *ptr, ptr);
+}
