8d077d63b6e59b49ae1e06499b80609750ea329b
EugeneViktP <popoff.zhenya@yandex.ru>
2025-07-01 18:18:21 +0300

    pointers to functions

--- funct_point.c
+++ funct_point.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void PrintHello(char *name)
+{
+    printf("Hello %s\n", name);
+}
+
 int Add(int a, int b)
 {
     return a + b;
@@ -8,6 +13,10 @@ int Add(int a, int b)
 
 int main(void)
 {
+    void(*ptr)(char*);
+    ptr = PrintHello;
+    ptr("Tom");
+    
     int c;
     //pointer to function
     int (*p)(int, int);
