b28839448fbcef68f7761d09821a2453a587eb2c
EugeneViktP <popoff.zhenya@yandex.ru>
2025-07-02 10:45:00 +0300

    callback updated

--- callback.c
+++ callback.c
@@ -2,48 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 
-int compare(int a, int b)
+int compare(const void* a, const void* b)
 {
-    if (a > b) 
-    {
-        return -1;
-    } else 
-    {
-        return 1;
-    }
-}
-
-int absulute_compare(int a, int b)
-{
-    if (abs(a) > abs(b))
-    {
-        return 1;
-    } else {
-        return -1;
-    }
-    
-}
-
-void BubbleSort(int *A, int n, int (*compare)(int, int))
-{
-    int i, j, temp;
-    for(i = 0; i < n; i++)
-    {
-        for(j = 0; j < n - 1; j++) {
-            if (compare(A[j], A[j + 1]) > 0) {  // compare A[j] with A[j + 1] and SWAP if needed
-            temp = A[j];
-            A[j] = A[j + 1];
-            A[j + 1] = temp;
-        }
-        }
-    }
+    int A = *((int*)a); // typecasting to int* and getting value
+    int B = *((int*)b);
+    return A-B;
 }
 
 int main(void)
 {
 
     int i, A[] = {-30, -40, 50, -6, -1, 4}; // sort in increasing order
-    BubbleSort(A, 6, absulute_compare);
+    qsort(A, 6, sizeof(int), compare );
     for (i = 0; i < 6; i++)
     {
         printf("%d ", A[i]);
