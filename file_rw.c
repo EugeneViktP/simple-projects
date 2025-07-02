9c14443d8bec243ae2c779191350a14feeafc1f0
EugeneViktP <popoff.zhenya@yandex.ru>
2025-07-02 19:26:34 +0300

    file handling

--- file_rw.c
+++ file_rw.c
@@ -3,9 +3,16 @@
 
 int main(void)
 {
-   FILE *fpointer = fopen("text.txt", "a");
+   char line[255];
+   
+   FILE *fpointer = fopen("text.txt", "r");
 //    fprintf(fpointer, "This line contains some information,\nthis is the second line,\nthis is the third lin of information");
-    fprintf(fpointer, "\nThis is fourth line of information");
+    // fprintf(fpointer, "\nThis is fourth line of information");
+
+    fgets(line, 255, fpointer);
+    fgets(line, 255, fpointer);
+    printf("%s", line);
+
 
     fclose(fpointer);
     return 0;
